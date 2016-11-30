bd81dfe6c3a0c7022ce265ab6fad256d3b18f406
Robert Rüdiger <ruerob@t-online.de>
2016-11-30 15:58:36 +0100

    center header

--- node.lua
+++ node.lua
@@ -80,8 +80,8 @@ function node.render()
     g=colors[1][2]
     b=colors[1][3]
     a=colors[1][4]
-    font:write(0,0,Config.get_header(),80,1,1,1,1)
-    --font:write(960-(font:width(Config.get_header(),80)/2),0,Config.get_header(),80,r,g,b,a)
+    
+    font:write(960-(font:width(Config.get_header(),80)/2),0,Config.get_header(),80,1,1,1,1)
     
     --write header
     write_line(0,100,"Raum","Tag","Uhrzeit","Fach","Lehrer")
