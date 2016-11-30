cafe97b7301f4a07279cd627ee4afe12c6cff203
Robert Rüdiger <ruerob@t-online.de>
2016-11-30 15:59:55 +0100

    color testing

--- node.lua
+++ node.lua
@@ -81,6 +81,8 @@ function node.render()
     b=colors[1][3]
     a=colors[1][4]
     
+    font:write(0,0,r,30,1,1,1,1)
+    
     font:write(960-(font:width(Config.get_header(),80)/2),0,Config.get_header(),80,1,1,1,1)
     
     --write header
