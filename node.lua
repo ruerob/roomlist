c9b5cdef0049c9800e169c951da48662bff7451b
Robert Rüdiger <ruerob@t-online.de>
2016-12-02 16:40:13 +0100

    debug

--- node.lua
+++ node.lua
@@ -237,6 +237,7 @@ function node.render()
         font:write(0,0,"(" .. page .. "/" .. #Config.get_roomlist() .. ")",font_size,1,1,1,1)
     end
     
+    font:write(0,50,((os.time()%8) + 1),40,1,1,1,1)
     color_blinker[(os.time()%8) + 1]:draw(0,0,1920,1080)
     
 end
