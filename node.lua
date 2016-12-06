7bbfaf374386a596b6e631b488bef9c450d62730
Robert Rüdiger <ruerob@t-online.de>
2016-12-06 08:52:29 +0100

    debugged

--- node.lua
+++ node.lua
@@ -169,20 +169,19 @@ function node.render()
     --clear the screen
     gl.clear(get_rgba(colors[1]))
     
-    scale = scale + (scale/4)
-    if(scale > 64) then scale = 0.01 end
-    
-    gl.translate((1920-1920*scale)/2, (1080-1080*scale)/2)
-   
     if ((os.time()%10) > 7) then
         if ((os.time()%10)>8) then
-            gl.scale(((os.time()%10)-8)*1,((os.time()%10)-8)*1)
+            scale = ((os.time()%10)-8)*1)
         else
-            gl.scale(((os.time()%10)-8)*32,((os.time()%10)-8)*32)
+            scale = ((os.time()%10)-7)*32
         end
     else
-        gl.scale(1,1)
+        scale=1
     end
+    
+    gl.translate((1920-1920*scale)/2, (1080-1080*scale)/2)
+    
+    gl.scale(scale,scale)
     
     logo:draw(624, 150, 1296, 1062, 0.15);
     
